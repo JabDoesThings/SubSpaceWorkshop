870885502d33be3320e865f66cee6dd8ef045f5a
JabJabjab <JabJabJab@Github.com>
2020-02-09 06:11:53 -0600

    Drafted & Implemented DataObject class.

---
+++ src/ts/map/DataObject.ts
@@ -0,0 +1,70 @@
+import uuid = require('uuid');
+
+/**
+ * The <i>DataObject</i> class. TODO: Document.
+ *
+ * @author Jab.
+ */
+export class DataObject {
+
+    private id: string;
+    private name: string;
+
+    /**
+     * Main constructor.
+     *
+     * @param name The decorative name of the object.
+     * @param id The internal ID of the object. If not provided, a UUID V4 is generated.
+     */
+    constructor(name: string, id: string = null) {
+        this.name = name;
+        if (id == null) {
+            this.id = uuid.v4();
+        } else {
+            this.id = id;
+        }
+    }
+
+    /**
+     * Simplified method for comparing two DataObjects by their respective ID's.
+     *
+     * @param other The other DataObject to compare.
+     *
+     * @return Returns 'true' if the DataObject's ID equals this ID.
+     */
+    public equals(other: DataObject): boolean {
+        return other != null && other.id === this.id;
+    }
+
+    /**
+     * @return Returns the decorative name of the object.
+     */
+    public getName(): string {
+        return this.name;
+    }
+
+    /**
+     * Sets the decorative name of the object.
+     *
+     * @param name The name to set.
+     */
+    public setName(name: string): void {
+        this.name = name;
+    }
+
+    /**
+     * @return Returns the internal ID of the object.
+     */
+    public getId(): string {
+        return this.id;
+    }
+
+    /**
+     * Sets the internal ID of the object.
+     *
+     * @param id The ID to set.
+     */
+    public setId(id: string): void {
+        this.id = id;
+    }
+}
