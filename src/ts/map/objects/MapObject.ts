5ff3d28f8837d034c24591e05f11c68f411a102c
JabJabjab <JabJabJab@Github.com>
2020-02-09 06:47:32 -0600

    Drafted inheritence code for MapObject.

--- src/ts/map/objects/MapObject.ts
+++ src/ts/map/objects/MapObject.ts
@@ -8,6 +8,9 @@ import { DataObject } from '../DataObject';
  */
 export class MapObject extends DataObject {
 
+    private parent: MapObject;
+    private children: MapObject[];
+
     private layer: MapLayer;
     private width: number;
     private height: number;
@@ -24,6 +27,7 @@ export class MapObject extends DataObject {
         super(name, id);
         this.width = width;
         this.height = height;
+        this.children = [];
     }
 
     public getLayer(): MapLayer {
@@ -48,6 +52,175 @@ export class MapObject extends DataObject {
 
     public setHeight(value: number): void {
         this.height = value;
+    }
+
+    /////////////////
+    // PARENT CODE //
+    /////////////////
+
+    public hasParent(): boolean {
+        return this.parent != null;
+    }
+
+    public isParent(object: MapObject): boolean {
+        return this.parent != null && this.parent.equals(object);
+    }
+
+    public getParent(): MapObject {
+        return this.parent;
+    }
+
+    public setParent(object: MapObject): void {
+
+        if (this.parent != null) {
+
+            // Make sure the object is not already the parent.
+            if (this.parent.equals(object)) {
+                throw new Error(
+                    "The object '"
+                    + object.getName()
+                    + "' ("
+                    + object.getId()
+                    + ") is already the parent of the object '"
+                    + this.getName()
+                    + "' ("
+                    + this.getId()
+                    + ")"
+                );
+            }
+
+            // Perform a secondary check to prevent infinite recursion.
+            if (this.parent.isChild(this)) {
+                this.parent.removeChild(this);
+            }
+        }
+
+        this.parent = object;
+    }
+
+    ///////////////////
+    // CHILDREN CODE //
+    ///////////////////
+
+    public hasChildren(): boolean {
+        return this.children.length != 0;
+    }
+
+    /**
+     *
+     * @param object
+     */
+    public isChild(object: MapObject): boolean {
+
+        // Make sure the object given is not null.
+        if(object == null) {
+            throw new Error("The object given is null or undefined.");
+        }
+
+        // Make sure that the object has children before iterating.
+        if (this.hasChildren()) {
+
+            // Go through each child to see if one matches the object given.
+            for (let key in this.children) {
+
+                // Grab the next child in the array.
+                let value = this.children[key];
+
+                // If the object's ID equals the next child, then we have
+                //   a match.
+                if (value.equals(object)) {
+                    return true;
+                }
+            }
+        }
+
+        // If we reached this line, no child matches the one given.
+        return false;
+    }
+
+    public addChild(object: MapObject): void {
+
+        // Make sure the object given is not null.
+        if(object == null) {
+            throw new Error("The object given is null or undefined.");
+        }
+
+        // Make sure the object is not already a child.
+        if (this.isChild(object)) {
+            throw new Error(
+                "The object '"
+                + object.getName()
+                + "' ("
+                + object.getId()
+                + ") is already a child of the object '"
+                + this.getName()
+                + " ("
+                + this.getId()
+                + ")"
+            );
+        }
+
+        // Perform a secondary check to prevent infinite recursion.
+        if (!object.isParent(this)) {
+            object.setParent(this);
+        }
+
+        // Add the child as the next index in the array.
+        this.children.push(object);
+
+    }
+
+    public removeChild(object: MapObject): void {
+
+        // Make sure the object given is not null.
+        if(object == null) {
+            throw new Error("The object given is null or undefined.");
+        }
+
+        // Make sure the object is a child.
+        if (!this.isChild(object)) {
+            throw new Error(
+                "The object '"
+                + object.getName()
+                + "' ("
+                + object.getId()
+                + ") is not a child of the object '"
+                + this.getName()
+                + " ("
+                + this.getId()
+                + ")"
+            );
+        }
+
+        // Perform a secondary check to prevent infinite recursion.
+        if (object.isParent(this)) {
+            object.setParent(null);
+        }
+
+        // Create the new array to replace the children array.
+        let newChildren: MapObject[] = [];
+
+        // Go through each child and check for a match.
+        for (let key in this.children) {
+
+            let value = this.children[key];
+
+            // If the child matches, skip adding it to the new array.
+            if (value.equals(object)) {
+                continue;
+            }
+
+            newChildren.push(value);
+
+        }
+
+        // Set the new children array.
+        this.children = newChildren;
+
+    }
+
+    public getChildren(): MapObject[] {
+        return this.children;
     }
 
 }
